1d3dd077568434fe3305549efa909c59332f946d
Thomas Reynaud <thomas.reynaud@clustree.com>
2017-10-08 00:15:29 +0200

    Add bias code snippet

--- bias_svd.py
+++ bias_svd.py
@@ -2,18 +2,25 @@ import numpy as np
 
 if __name__ == '__main__':
     R = np.mat([[1, 1, 1],
-                [7, 7, 7],
-                [3, 1, 1],
-                [5, 7, 7],
-                [3, 1, np.NA],
-                [5, 7, np.NA],
-                [3, 1, np.NA],
-                [5, 7, np.NA],
-                [3, 1, np.NA],
-                [5, 7, np.NA],
-                [3, 1, np.NA],
-                [5, 4, np.NA]])
-    
-    sim = np.dot(R.transpose(), R)
+                [10, 10, 10],
+                [5, 1, 1],
+                [7, 10, 10],
+                [5, 1, np.nan],
+                [7, 10, np.nan],
+                [5, 1, np.nan],
+                [7, 10, np.nan],
+                [5, 1, np.nan],
+                [7, 10, np.nan],
+                [5, 1, np.nan],
+                [7, 10, np.nan]])
 
-    print(sim)
+    # Robust covariance
+    R_masked = np.ma.masked_invalid(R)
+    print(np.ma.cov(R_masked, rowvar=False, allow_masked=True))
+
+    # Replace missing values by the mean of the column
+    mean = np.nanmean(R[:, 2])
+    inds = np.where(np.isnan(R))
+    R[inds] = mean
+
+    print(np.cov(R, rowvar=False))
